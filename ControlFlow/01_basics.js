21bee75755c657e064bd45a4b49bdb206906923a
Sachin9316 <su9206407@gmail.com>
2024-10-07 21:39:41 +0530

    commit

---
+++ ControlFlow/01_basics.js
@@ -0,0 +1,22 @@
+// < , > , <= , >= , == , === , !=
+
+// const isUserLoggedIn = true;
+// const temp = 41
+
+// if (temp === 41) {
+//   console.log("Temprature less then 50");
+// } else {
+//   console.log("Greater than 50");
+// }
+
+// const score = 200;
+
+// if(score > 100) {
+//   const power = "fly"
+//   console.log(power);
+// }
+
+const balance = 1000;
+
+// if(balance < 2000) console.log("test");
+
---
+++ ControlFlow/02_TruthyFlasy.js
@@ -0,0 +1,9 @@
+// Falsy Value 
+// ---> false , 0, -0 , BigInt 0n , null , "" , undefined , NaN
+
+// Truthy Value
+// ---> true , "0" , [] , 'false',  {} , 1 , 1n , Infinity , -Infinity , 1.0 
+
+const price = 200;
+(price < 180) ? console.log("GEt It For 180") : console.log("Get  it for 200");
+7600
